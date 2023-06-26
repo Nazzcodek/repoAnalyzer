2c743b9eaded649674583222213f117babc354b0
Nazzcodek <65385684+Nazzcodek@users.noreply.github.com>
2023-06-26 18:05:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ def analyze_repos(repo_info):
     # Load the CSV file and generate embeddings using Cohere API
     loader = CSVLoader(file_path="repo_data.csv", encoding="utf-8")
     csv_data = loader.load()
-    csv_embeddings = CohereEmbeddings(co)
+    csv_embeddings = CohereEmbeddings()
     vectors = Qdrant.from_documents(csv_data, csv_embeddings)
     # Define the context and prompt templates for the PromptSession
     context = """
